bb81e52f7aa72cfbecb75fbda0503e875eebef54
79872 <79872@ERRORES>
2023-06-26 16:24:48 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace Meter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MainFileButton = new System.Windows.Forms.Button();
+            this.mainFileTextBox = new System.Windows.Forms.TextBox();
+            this.AdditionalFileButton = new System.Windows.Forms.Button();
+            this.AddAdditionalFileButton = new System.Windows.Forms.Button();
+            this.ResFileTextBox = new System.Windows.Forms.TextBox();
+            this.ResFIleButton_Click = new System.Windows.Forms.Button();
+            this.MergeButton = new System.Windows.Forms.Button();
+            this.SepButton = new System.Windows.Forms.Button();
+            this.additionalFilesListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Основной файл";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Файлы РЭСов";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Разделение по РЭС";
+            // 
+            // MainFileButton
+            // 
+            this.MainFileButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MainFileButton.BackgroundImage")));
+            this.MainFileButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.MainFileButton.Location = new System.Drawing.Point(639, 25);
+            this.MainFileButton.Name = "MainFileButton";
+            this.MainFileButton.Size = new System.Drawing.Size(34, 23);
+            this.MainFileButton.TabIndex = 3;
+            this.MainFileButton.UseVisualStyleBackColor = true;
+            this.MainFileButton.Click += new System.EventHandler(this.MainFIleButton_Click);
+            // 
+            // mainFileTextBox
+            // 
+            this.mainFileTextBox.Location = new System.Drawing.Point(200, 25);
+            this.mainFileTextBox.Name = "mainFileTextBox";
+            this.mainFileTextBox.Size = new System.Drawing.Size(433, 22);
+            this.mainFileTextBox.TabIndex = 4;
+            // 
+            // AdditionalFileButton
+            // 
+            this.AdditionalFileButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("AdditionalFileButton.BackgroundImage")));
+            this.AdditionalFileButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.AdditionalFileButton.Location = new System.Drawing.Point(639, 66);
+            this.AdditionalFileButton.Name = "AdditionalFileButton";
+            this.AdditionalFileButton.Size = new System.Drawing.Size(34, 23);
+            this.AdditionalFileButton.TabIndex = 8;
+            this.AdditionalFileButton.UseVisualStyleBackColor = true;
+            this.AdditionalFileButton.Click += new System.EventHandler(this.AdditionalFileButton_Click);
+            // 
+            // AddAdditionalFileButton
+            // 
+            this.AddAdditionalFileButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("AddAdditionalFileButton.BackgroundImage")));
+            this.AddAdditionalFileButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.AddAdditionalFileButton.Location = new System.Drawing.Point(679, 66);
+            this.AddAdditionalFileButton.Name = "AddAdditionalFileButton";
+            this.AddAdditionalFileButton.Size = new System.Drawing.Size(34, 23);
+            this.AddAdditionalFileButton.TabIndex = 9;
+            this.AddAdditionalFileButton.UseVisualStyleBackColor = true;
+            this.AddAdditionalFileButton.Click += new System.EventHandler(this.AddAdditionalFileButton_Click);
+            // 
+            // ResFileTextBox
+            // 
+            this.ResFileTextBox.Location = new System.Drawing.Point(200, 138);
+            this.ResFileTextBox.Name = "ResFileTextBox";
+            this.ResFileTextBox.Size = new System.Drawing.Size(433, 22);
+            this.ResFileTextBox.TabIndex = 10;
+            // 
+            // ResFIleButton_Click
+            // 
+            this.ResFIleButton_Click.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ResFIleButton_Click.BackgroundImage")));
+            this.ResFIleButton_Click.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ResFIleButton_Click.Location = new System.Drawing.Point(639, 138);
+            this.ResFIleButton_Click.Name = "ResFIleButton_Click";
+            this.ResFIleButton_Click.Size = new System.Drawing.Size(34, 23);
+            this.ResFIleButton_Click.TabIndex = 11;
+            this.ResFIleButton_Click.UseVisualStyleBackColor = true;
+            this.ResFIleButton_Click.Click += new System.EventHandler(this.ResFIleButton_Click_Click);
+            // 
+            // MergeButton
+            // 
+            this.MergeButton.Location = new System.Drawing.Point(357, 94);
+            this.MergeButton.Name = "MergeButton";
+            this.MergeButton.Size = new System.Drawing.Size(102, 23);
+            this.MergeButton.TabIndex = 12;
+            this.MergeButton.Text = "Объединить";
+            this.MergeButton.UseVisualStyleBackColor = true;
+            this.MergeButton.Click += new System.EventHandler(this.MergeButton_Click);
+            // 
+            // SepButton
+            // 
+            this.SepButton.Location = new System.Drawing.Point(357, 166);
+            this.SepButton.Name = "SepButton";
+            this.SepButton.Size = new System.Drawing.Size(102, 23);
+            this.SepButton.TabIndex = 13;
+            this.SepButton.Text = "Разделить";
+            this.SepButton.UseVisualStyleBackColor = true;
+            this.SepButton.Click += new System.EventHandler(this.SepButton_Click);
+            // 
+            // additionalFilesListBox
+            // 
+            this.additionalFilesListBox.FormattingEnabled = true;
+            this.additionalFilesListBox.ItemHeight = 16;
+            this.additionalFilesListBox.Location = new System.Drawing.Point(200, 66);
+            this.additionalFilesListBox.Name = "additionalFilesListBox";
+            this.additionalFilesListBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.additionalFilesListBox.Size = new System.Drawing.Size(433, 20);
+            this.additionalFilesListBox.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(722, 192);
+            this.Controls.Add(this.additionalFilesListBox);
+            this.Controls.Add(this.SepButton);
+            this.Controls.Add(this.MergeButton);
+            this.Controls.Add(this.ResFIleButton_Click);
+            this.Controls.Add(this.ResFileTextBox);
+            this.Controls.Add(this.AddAdditionalFileButton);
+            this.Controls.Add(this.AdditionalFileButton);
+            this.Controls.Add(this.mainFileTextBox);
+            this.Controls.Add(this.MainFileButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button MainFileButton;
+        private System.Windows.Forms.TextBox mainFileTextBox;
+        private System.Windows.Forms.Button AdditionalFileButton;
+        private System.Windows.Forms.Button AddAdditionalFileButton;
+        private System.Windows.Forms.TextBox ResFileTextBox;
+        private System.Windows.Forms.Button ResFIleButton_Click;
+        private System.Windows.Forms.Button MergeButton;
+        private System.Windows.Forms.Button SepButton;
+        private System.Windows.Forms.ListBox additionalFilesListBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Meter
+{
+    public partial class Form1 : Form
+    {
+        private string mainFilePath;
+        private List<string> additionalFilePaths;
+        public Form1()
+        {
+            InitializeComponent();
+            additionalFilePaths = new List<string>();
+        }
+
+        //Объединение
+        private void MergeExcelFiles(string outputFilePath)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = false;
+
+            try
+            {
+                // Создание нового файла
+                Excel.Workbook newWorkbook = excelApp.Workbooks.Add();
+                Excel.Worksheet newWorksheet = newWorkbook.ActiveSheet;
+
+                int newRow = 1;
+
+                // Открытие основного файла Excel
+                Excel.Workbook mainWorkbook = excelApp.Workbooks.Open(mainFilePath);
+                Excel.Worksheet mainWorksheet = mainWorkbook.ActiveSheet;
+                Excel.Range mainRange = mainWorksheet.UsedRange;
+
+                // Копирование строк из основного файла в новый файл
+                Excel.Range sourceRange = mainWorksheet.UsedRange;
+                Excel.Range targetRange = newWorksheet.Cells[newRow, 1];
+
+                sourceRange.Copy(targetRange);
+                newRow += sourceRange.Rows.Count;
+
+                // Словарь для отслеживания уникальных записей в основном файле
+                Dictionary<string, bool> uniqueRecords = new Dictionary<string, bool>();
+
+                // Перебор строк в основном файле и добавление их в словарь
+                int mainRowCount = mainRange.Rows.Count;
+                int mainColumnCount = mainRange.Columns.Count;
+
+                for (int row = 4; row <= mainRowCount; row++)
+                {
+                    string uniqueValueC = mainWorksheet.Cells[row, 3].Value.ToString();
+                    string uniqueValueD = mainWorksheet.Cells[row, 4].Value.ToString();
+                    string uniqueValueS = mainWorksheet.Cells[row, 19].Value.ToString();
+
+                    string uniqueKey = uniqueValueC + uniqueValueD + uniqueValueS;
+                    if (!uniqueRecords.ContainsKey(uniqueKey))
+                    {
+                        uniqueRecords.Add(uniqueKey, true);
+                    }
+                }
+
+                // Перебор дополнительных файлов Excel
+                foreach (string additionalFilePath in additionalFilePaths)
+                {
+                    Excel.Workbook additionalWorkbook = excelApp.Workbooks.Open(additionalFilePath);
+                    Excel.Worksheet additionalWorksheet = additionalWorkbook.ActiveSheet;
+                    Excel.Range additionalRange = additionalWorksheet.UsedRange;
+
+                    int rowCount = additionalRange.Rows.Count;
+                    int columnCount = additionalRange.Columns.Count;
+
+                    // Перебор строк в дополнительном файле
+                    for (int row = 4; row <= rowCount; row++)
+                    {
+                        string uniqueValueC = additionalWorksheet.Cells[row, 3].Value.ToString();
+                        string uniqueValueD = additionalWorksheet.Cells[row, 4].Value.ToString();
+                        string uniqueValueS = additionalWorksheet.Cells[row, 19].Value.ToString();
+
+                        string uniqueKey = uniqueValueC + uniqueValueD + uniqueValueS;
+                        if (!uniqueRecords.ContainsKey(uniqueKey))
+                        {
+                            // Добавление уникальной записи в новый файл
+                            Excel.Range sourceDataRange = additionalWorksheet.Range[string.Format("A{0}:Z{0}", row)];
+                            Excel.Range targetDataRange = newWorksheet.Range[string.Format("A{0}", newRow)];
+                            sourceDataRange.Copy(targetDataRange);
+                            newRow++;
+
+                            // Добавление записи в словарь уникальных записей
+                            uniqueRecords.Add(uniqueKey, true);
+                        }
+                    }
+
+                    additionalWorkbook.Close();
+                }
+
+                // Сохранение изменений в новом файле
+                newWorkbook.SaveAs(outputFilePath);
+                newWorkbook.Close();
+
+                MessageBox.Show("Файлы успешно объединены!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при объединении файлов Excel: " + ex.Message);
+            }
+            finally
+            {
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            }
+        }
+
+        private void MainFIleButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xlsx;*.xls";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                mainFilePath = openFileDialog.FileName;
+                mainFileTextBox.Text = mainFilePath;
+            }
+        }
+
+        private void AdditionalFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xlsx;*.xls";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string additionalFilePath = openFileDialog.FileName;
+                additionalFilePaths.Add(additionalFilePath);
+                additionalFilesListBox.Items.Add(additionalFilePath);
+            }
+        }
+
+        private void AddAdditionalFileButton_Click(object sender, EventArgs e)
+        {
+            {
+                AdditionalFileButton_Click(sender, e);
+            }
+        }
+
+        private void MergeButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel Files|*.xlsx;*.xls";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string outputFilePath = saveFileDialog.FileName;
+                MergeExcelFiles(outputFilePath);
+            }
+        }
+
+        //Разделение
+
+        private string inputFilePath;
+        private void SepExcelFiles(string inputFile)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = false;
+
+            try
+            {
+                // Открытие исходного файла Excel
+                Excel.Workbook mainWorkbook = excelApp.Workbooks.Open(inputFile);
+                Excel.Worksheet mainWorksheet = mainWorkbook.ActiveSheet;
+
+                // Получение диапазона данных из исходного файла
+                Excel.Range mainRange = mainWorksheet.UsedRange;
+
+                // Получение уникальных значений в столбце E
+                Excel.Range uniqueValuesRange = mainWorksheet.Range["E3:E" + mainWorksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row];
+
+                // Перебор уникальных значений и создание новых файлов
+                foreach (Excel.Range cell in uniqueValuesRange)
+                {
+                    string uniqueValue = cell.Value.ToString();
+
+                    // Создание нового файла
+                    Excel.Workbook newWorkbook = excelApp.Workbooks.Add();
+                    Excel.Worksheet newWorksheet = newWorkbook.ActiveSheet;
+
+                    // Копирование первых трех строк из исходного файла
+                    Excel.Range sourceHeaderRange = mainWorksheet.Range["A1:Z3"];
+                    Excel.Range targetHeaderRange = newWorksheet.Range["A1"];
+                    sourceHeaderRange.Copy(targetHeaderRange);
+                    int newRow = 4;
+
+                    // Перебор строк исходного файла и добавление строк с одинаковыми значениями в ячейках E в новый файл
+                    foreach (Excel.Range row in mainRange.Rows)
+                    {
+                        string currentValue = row.Cells[1, 5].Value.ToString(); // Значение в столбце E
+
+                        if (currentValue == uniqueValue)
+                        {
+                            row.Copy(newWorksheet.Cells[newRow, 1]);
+                            newRow++;
+                        }
+                    }
+
+                    // Сохранение нового файла с уникальным именем
+                    string outputFileName = Path.GetFileNameWithoutExtension(inputFile) + "_" + uniqueValue;
+                    string outputFilePath = Path.Combine(Path.GetDirectoryName(inputFile), outputFileName + ".xlsx");
+                    newWorkbook.SaveAs(outputFilePath);
+                    newWorkbook.Close();
+                }
+
+                mainWorkbook.Close();
+
+                MessageBox.Show("Файл успешно разделен на группы!");
+            }
+            catch (Exception ex)
+            {
+                // Обработка ошибок
+                MessageBox.Show("Произошла ошибка при разделении файла Excel: " + ex.Message);
+            }
+            finally
+            {
+                // Закрытие приложения Excel
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            }
+        }
+
+        private void ResFIleButton_Click_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xlsx;*.xls";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                inputFilePath = openFileDialog.FileName;
+                ResFileTextBox.Text = inputFilePath;
+            }
+        }
+
+        private void SepButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(inputFilePath))
+            {
+                SepExcelFiles(inputFilePath);
+
+                MessageBox.Show("Файл успешно разделен на группы!");
+            }
+            else
+            {
+                MessageBox.Show("Выберите исходный файл!");
+            }
+        }
+    }
+}
